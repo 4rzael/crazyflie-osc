46cba482dc71c0e171fbb7c68d6d589ae0f26238
4rzael <maxime.agor23@gmail.com>
2017-08-18 20:19:55 +0200

    Fixed windows launcher virtualenv and now uses my cflib until we can change the packet loss threshold in the official one

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @ECHO off
 
-cd src
+
 set /p ip="IP : (default=0.0.0.0) :"
 set /p metaport="META PORT : (default=5006) :"
 set /p innerport="INNER PORT : (default=5005) :"
@@ -9,7 +9,6 @@ IF /i "%ip%" == "" set ip="0.0.0.0"
 IF /i "%metaport%" == "" set metaport="5006"
 IF /i "%innerport%" == "" set innerport="5005"
 
-venv\Scripts\Activate
-
-python ./server.py --ip %ip% --meta_port %metaport% --inner_port %innerport%
+cd src
+..\venv\Scripts\python ./server.py --ip %ip% --meta_port %metaport% --inner_port %innerport%
 PAUSE
